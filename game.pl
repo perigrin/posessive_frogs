1bf63fd044d79810dbfd2ab019e853935b8ade31
Chris Prather <chris.prather@reopenlabs.com>
2023-06-19 22:27:41 -0400

    part 1
    Getting our @ to show up on the screen and move around.

--- game.pl
+++ game.pl
@@ -2,13 +2,26 @@
 use 5.38.0;
 
 use lib qw(lib);
-use experimental  'class';
+use experimental 'class';
 
 use Games::ROT;
+
+class QuitAction { }
+
+class MovementAction {
+    field $dx :param //= 0;
+    field $dy :param //= 0;
+
+    method dx { $dx }
+    method dy { $dy }
+}
 
 class Engine {
     my $WIDTH = 80;
     my $HEIGHT = 50;
+
+    field $player_x = $WIDTH / 2;
+    field $player_y = $WIDTH / 2;
 
     field $app = Games::ROT->new(
         screen_width  => $WIDTH,
@@ -16,14 +29,25 @@ class Engine {
     );
 
     ADJUST {
+        $app->add_event_handler(
+            'keydown' => sub ($event) {
+                my %KEY_MAP = (
+                    h => sub { $player_x -= 1 },
+                    j => sub { $player_y += 1 },
+                    k => sub { $player_y -= 1 },
+                    l => sub { $player_x += 1 },
+                    q => sub { exit }
+                );
+                # lets execute the action now
+                $KEY_MAP{$event->key}->();
+            }
+        );
         $app->run( sub { $self->render() } );
     }
 
     method render() {
-        my $x = $WIDTH / 2;
-        my $y = $HEIGHT / 2;
-
-        $app->draw($x, $y, 'Hello World', '#fff', '#000');
+        $app->clear();
+        $app->draw($player_x, $player_y, '@', '#fff', '#000');
     }
 }
 
